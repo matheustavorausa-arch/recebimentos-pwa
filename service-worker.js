c61265b9cd0410548d81f47211b878369fd34efd
Matheus Tavora <matheustavorausa-arch@users.noreply.github.com>
2026-07-08 09:16:55 -0700

    Add real menu link navigation

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'recebimentos-v29';
-const ASSETS = ['./','./index.html','./css/styles.css?v=29','./css/betting-theme.css?v=29','./js/payment-history-data.js?v=29','./js/app.js?v=29','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE = 'recebimentos-v30';
+const ASSETS = ['./','./index.html','./css/styles.css?v=30','./css/betting-theme.css?v=30','./js/payment-history-data.js?v=30','./js/app.js?v=30','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', event => {
