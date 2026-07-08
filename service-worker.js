e31e704d003b2977f6059a9a1aee80a278c049fa
Matheus Tavora <matheustavorausa-arch@users.noreply.github.com>
2026-07-08 15:41:58 -0700

    Improve light theme contrast

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'recebimentos-v39';
-const ASSETS = ['./','./index.html','./css/styles.css?v=39','./css/betting-theme.css?v=39','./js/payment-history-data.js?v=39','./js/app.js?v=39','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE = 'recebimentos-v40';
+const ASSETS = ['./','./index.html','./css/styles.css?v=40','./css/betting-theme.css?v=40','./js/payment-history-data.js?v=40','./js/app.js?v=40','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', event => {
